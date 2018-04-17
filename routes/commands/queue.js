9bf3d356fe431969e9da1c1c2725c8da57f816d4
Casey Olson <casey@bookcasey.com>
2018-04-17 17:02:47 -0500

    convert queue to user_id, tests for queue

--- routes/commands/queue.js
+++ routes/commands/queue.js
@@ -2,11 +2,16 @@ const express = require('express');
 const moment = require('moment-timezone');
 const router = express.Router();
 
+const { SLACK_VERIFICATION_TOKEN } = require('../../config');
+
 const { Ticket } = require('../../models');
 
 router.post('/', (req, res, next) => {
-  const { channel_id } = req.body;
-  const today = moment().startOf('day');
+  const { channel_id, token } = req.body;
+
+  if(token !== SLACK_VERIFICATION_TOKEN) throw 'Unauthorized';
+
+  const today = moment().tz('America/New_York').startOf('day');
   const tomorrow = moment(today).add(1, 'days');
   if(!channel_id) {
     return next('A channel ID is required.');
@@ -20,7 +25,10 @@ router.post('/', (req, res, next) => {
     .sort({created_at: 1})
     .then(tickets => {
       if(tickets.length <= 0) return Promise.reject('No sessions in queue');
-      res.send(tickets.map((ticket, index) => `[${index + 1}] ${ticket.owlSession} reported by <@${ticket.by}>.\n`).join(''));
+      res.json({
+        response_type: 'ephemeral',
+        text: tickets.map((ticket, index) => `[${index + 1}] ${ticket.owlSession} reported by <@${ticket.by}>.\n`).join('')
+      });
     })
     .catch(err => next(err));
 });
---
+++ test/routes/queue.test.js
@@ -0,0 +1,91 @@
+const app = require('../../index');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const moment = require('moment-timezone');
+
+const { SLACK_VERIFICATION_TOKEN } = require('../../config');
+
+const { Ticket } = require('../../models');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('TATickets - /queue', function() {
+  describe('POST /queue', function() {
+    it('can return a list of tickets', function() {
+      const today = moment().tz('America/New_York').startOf('day');
+      const tomorrow = moment(today).add(1, 'days');
+
+      return Ticket.find({
+        channelId: 'G9AJF01BL',
+        mentor: null,
+        created_at: { $gte: today.toDate(), $lt: tomorrow.toDate() },
+        isActive: true
+      }).then(function(tickets) {
+        const slackRequest = {
+          channel_id: 'G9AJF01BL',
+          user_id: 'UMENTOR2',
+          response_url: 'http://localhost:8080/test',
+          token: SLACK_VERIFICATION_TOKEN
+        };
+        return chai.request(app).post('/queue').send(slackRequest);
+      }).then(function(res) {
+        const body = res.body;
+  			expect(res).to.have.status(200);
+  			expect(res).to.be.json;
+  			expect(body).to.be.a('object');
+  			expect(body).to.include.keys('response_type', 'text');
+  			expect(body.response_type).to.equal('ephemeral');
+  			expect(body.text).to.equal('[1] https://sessions.thinkful.com/test8 reported by <@UTEST8>.\n[2] https://sessions.thinkful.com/test10 reported by <@UTEST10>.\n');
+      });
+    });
+    it('provides feedback if there are no sessions', function() {
+      const slackRequest = {
+        channel_id: 'FF014445',
+        user_id: 'UMENTOR2',
+        response_url: 'http://localhost:8080/test',
+        token: SLACK_VERIFICATION_TOKEN
+      };
+      return chai.request(app).post('/queue').send(slackRequest)
+        .then(function(res) {
+          const body = res.body;
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(body).to.be.a('object');
+          expect(body).to.include.keys('response_type', 'text');
+          expect(body.response_type).to.equal('ephemeral');
+          expect(body.text).to.equal('No sessions in queue');
+        });
+    });
+    it('rejects requests without channel id', function() {
+      const slackRequest = {
+        user_id: 'UMENTOR2',
+        response_url: 'http://localhost:8080/test',
+        token: SLACK_VERIFICATION_TOKEN
+      };
+      return chai.request(app).post('/queue').send(slackRequest)
+        .then(function(res) {
+          const body = res.body;
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(body).to.be.a('object');
+          expect(body).to.include.keys('response_type', 'text');
+          expect(body.response_type).to.equal('ephemeral');
+          expect(body.text).to.equal('A channel ID is required.');
+        });
+    });
+    it('rejects unauthorized requests', function() {
+      const slackRequest = {
+        channel_id: 'G9AJF01BL',
+        user_id: 'UMENTOR2',
+        response_url: 'http://localhost:8080/test',
+      };
+      return chai.request(app).post('/queue').send(slackRequest)
+        .then(function(res) {
+          const body = res.body;
+          expect(res).to.have.status(401);
+        });
+    });
+  });
+});
